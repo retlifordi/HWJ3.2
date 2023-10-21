5de8372ead94627afc0220602bc442f2044bdd1e
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-10-21 20:25:03 +0300

    all corrected parameters

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double height = 1.83;
-        int wt = 65;
-        int bmi = service.calculate(height, wt); // получилось 19
+        int weight = 65;
+        int bmi = service.calculate(height, weight); // получилось 19
         System.out.println(bmi);
     }
 }
