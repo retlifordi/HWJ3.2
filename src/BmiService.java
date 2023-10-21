0fec97d88c1b3b7dd4efadc66483d53af64ccff7
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-10-21 20:21:30 +0300

    corrected parameters

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
-    public int calculate(double hight, int wt) {
-        double bimi;
-        bimi = wt / (hight * hight);
+    public int calculate(double height, int weight) {
+        double bimi = weight / (height * height);
         return (int) bimi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double hight = 1.83;
+        double height = 1.83;
         int wt = 65;
-        int bmi = service.calculate(hight, wt); // получилось 19 
+        int bmi = service.calculate(height, wt); // получилось 19
         System.out.println(bmi);
     }
 }
